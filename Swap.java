e93623508195467f3f0a6587010ef9bb44f3cc7e
BSGandhi <86193461+BSGandhi@users.noreply.github.com>
2021-06-20 22:52:39 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,11 @@
+public class Swap {
+	public static void main(String []args) {
+	  int tmp = 0;
+		int a = 10;
+		int b = 20;
+		tmp = a;
+		a = b;
+		b = temp;
+		System.out.print(a+":"+b);
+	}
+}
